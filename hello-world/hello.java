733ed5e083453b93e518aab6419b7c95fd488806
luisgj2805 <luisgjoka54@gmail.com>
2021-12-21 15:51:09 +0100

    adding loop from java

---
+++ hello-world/hello.java
@@ -0,0 +1,11 @@
+class hellojava
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello world from Java!");
+        
+        for (int i = 0; i <= 2; i++)
+            System.out.println("Nice to meet you!");
+    }
+}
+
